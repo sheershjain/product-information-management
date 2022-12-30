dbedc100718ddc1cbe6d0d38be7e48471cae65b3
tanu-sh1201 <tanu@gkmit.co>
2022-12-30 17:07:16 +0530

    route for user

---
+++ routes/admin.route.js
@@ -0,0 +1,59 @@
+const { Router } = require("express");
+const controller = require("../controller");
+const { checkAccessToken } = require("../middleware/auth");
+const { checkRole } = require("../middleware/check-role");
+const validator = require("../validators");
+const genericResponse = require("../helper/generic-response.helper");
+const serialization = require("../serialization");
+const router = Router();
+
+router.post(
+  "/email-file",
+  checkAccessToken,
+    checkRole,
+  validator.userValidator.emailFileSchema,
+  controller.User.emailFile,
+  genericResponse.sendResponse
+);
+
+router.get(
+  "/get-email-data/:email",
+  checkAccessToken,
+  checkRole,
+  validator.userValidator.mailParse,
+  controller.User.mailParse,
+  genericResponse.sendResponse
+);
+router.get(
+  "/get-data",
+  checkAccessToken,
+  checkRole,
+  controller.User.getData,
+  genericResponse.sendResponse
+);
+router.post(
+  "/create-user",
+  checkAccessToken,
+  checkRole,
+  controller.User.createUser,
+  serialization.userSerialization.createUser,
+  genericResponse.sendResponse
+);
+
+router.delete(
+  "deactivate-user",
+  checkAccessToken,
+  checkRole,
+  controller.User.deactivateUser,
+  genericResponse.sendResponse
+)
+router.get(
+  "/user-details/:id",
+  checkAccessToken,
+  checkRole,
+  controller.User.detailById,
+  serialization.userSerialization.userDetail,
+  genericResponse.sendResponse
+)
+
+module.exports = router;
