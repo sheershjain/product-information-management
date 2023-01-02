28c391a278021d04cf8d9a4e1653643d2974018c
tanu-sh1201 <tanu@gkmit.co>
2023-01-02 15:25:09 +0530

    change in user model

--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
       role: {
-        type: DataTypes.STRING,
+        type: DataTypes.ENUM(['SADM','ADM','USR']),
         allowNull: false,
         defaultValue:'USR'
       },
