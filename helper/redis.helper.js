420adcbb882bd0d67d37bbb873abd219499141aa
tanu-sh1201 <tanu@gkmit.co>
2023-01-04 23:17:15 +0530

    change for deployment

--- helper/redis.helper.js
+++ helper/redis.helper.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const redis = require("redis");
 
-const getClient = () => {
-  return redis.createClient(process.env.REDIS_HOST, process.env.REDIS_PORT);
+const getClient =  () => {
+  return redis.createClient({
+    url: 'redis://default:08T0I17l1UDh4jnm68IPuSADrvCObuW5@redis-14737.c274.us-east-1-3.ec2.cloud.redislabs.com:14737'
+  });
 };
 
-let client = getClient();
-const connect = () => {
-  client.connect();
+let client =  getClient();
+const connect = async() => {
+  await client.connect();
 };
 
 const get = async (key) => {
